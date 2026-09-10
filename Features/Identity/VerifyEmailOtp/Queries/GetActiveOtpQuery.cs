5f189140df85c9e70016fcc3588fda9a4c8be21d
Khader Mohamed <khaderelhabal923@gmail.com>
2026-09-10 18:31:30 +0300

    feat(verify-otp): add queries and handlers to fetch OTP and user data

---
+++ Features/Identity/VerifyEmailOtp/Queries/GetActiveOtpQuery.cs
@@ -0,0 +1,5 @@
+﻿using exam_system.Features.Identity.VerifyEmailOtp.DTOs.Internal;
+
+namespace exam_system.Features.Identity.VerifyEmailOtp.Queries;
+
+public record GetActiveOtpQuery(string Email) : IRequest<OtpLookupResult?>;
