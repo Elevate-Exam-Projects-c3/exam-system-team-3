7a3ad28da08475404f61632d5a78e5d306331f6e
Khader Mohamed <khaderelhabal923@gmail.com>
2026-09-14 18:40:15 +0300

    feat(EXAM-X): add resend OTP controller endpoint

--- Features/Identity/ResendOtp/Controllers/ResendOtpController.cs
+++ Features/Identity/ResendOtp/Controllers/ResendOtpController.cs
@@ -1,6 +1,27 @@
-﻿namespace exam_system.Features.Identity.ResendOtp.Controllers;
+﻿using exam_system.Features.Identity.ResendOtp.DTOs.Request;
+using exam_system.Features.Identity.ResendOtp.DTOs.Response;
+using exam_system.Features.Identity.ResendOtp.Orchestrators;
+using exam_system.Features.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-public class ResendOtpController
+namespace exam_system.Features.Identity.ResendOtp.Controllers;
+
+[ApiController]
+[Route("api/identity")]
+public class ResendOtpController(IMediator mediator) : ControllerBase
 {
-    
+    [AllowAnonymous]
+    [HttpPost("resend-otp")]
+    public async Task<ActionResult<ApiResponse<ResendOtpResponse>>> Resend(
+        [FromBody] ResendOtpRequest request,
+        CancellationToken cancellationToken)
+    {
+        var orchestrator = new ResendOtpOrchestrator(request.Email);
+
+        var result = await mediator.Send(orchestrator, cancellationToken);
+        var response = result.ToApiResponse();
+
+        return StatusCode(response.StatusCode, response);
+    }
 }
