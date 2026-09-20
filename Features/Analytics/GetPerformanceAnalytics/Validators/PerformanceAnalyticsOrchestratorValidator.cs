c18ef6ae7619f9205d5cf7f6ec39389cd503c475
Khader Mohamed <khaderelhabal923@gmail.com>
2026-09-20 16:37:09 +0300

    feat(analytics): add date range validator for analytics orchestrator

--- Features/Analytics/GetPerformanceAnalytics/Validators/PerformanceAnalyticsOrchestratorValidator.cs
+++ Features/Analytics/GetPerformanceAnalytics/Validators/PerformanceAnalyticsOrchestratorValidator.cs
@@ -1,6 +1,14 @@
-﻿namespace exam_system.Features.Analytics.GetPerformanceAnalytics.Validators;
+﻿using exam_system.Features.Analytics.GetPerformanceAnalytics.Orchestrators;
+
+namespace exam_system.Features.Analytics.GetPerformanceAnalytics.Validators;
 
 public class PerformanceAnalyticsOrchestratorValidator
+    : AbstractValidator<PerformanceAnalyticsOrchestrator>
 {
-    
+    public PerformanceAnalyticsOrchestratorValidator()
+    {
+        RuleFor(x => x)
+            .Must(x => x.DateFrom is null || x.DateTo is null || x.DateFrom <= x.DateTo)
+            .WithMessage("'dateFrom' must be earlier than or equal to 'dateTo'.");
+    }
 }
